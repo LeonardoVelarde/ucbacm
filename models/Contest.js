f4510caa0737f9643dbb8b433a6206da1a0d7b94
Leonardo <leonardo.velarde.ldvc@gmail.com>
2016-07-19 19:03:46 -0400

    added basic contest initialization

--- models/Contest.js
+++ models/Contest.js
@@ -1,7 +1,10 @@
-var Contest = function(){
+var Contest = function(name, contestDate, category){
   if(!(this instanceof Contest)){
-    return new Contest();
+    return new Contest(name, contestDate, category);
   }
+  this.name = name;
+  this.contestDate = contestDate;
+  this.category = category;
 };
 
 module.exports = Contest;
--- tests/contest.test.js
+++ tests/contest.test.js
@@ -1,12 +1,32 @@
 var Contest = require('../models/Contest');
 var assert = require('chai').assert;
 
+function createContest(){
+  return new Contest('First Contest', new Date(2016, 3, 15), 'Advanced' );
+}
+
 describe('Contest', function() {
 
   it('should create a non null Contest object', function() {
-    var newContest = new Contest();
+    var newContest = createContest();
     assert.isNotNull(newContest);
     assert.instanceOf(newContest, Contest);
+  });
+
+  it('should create a named contest and retrieve its name', function() {
+    var newContest = createContest();
+    assert.equal(newContest.name, 'First Contest');
+  });
+
+  it('should create a contest with date and retrieve that date', function() {
+    var contestDate = new Date(2016, 3, 15);
+    var newContest = new Contest('First Contest', new Date(2016, 3, 15) );
+    assert.equal(newContest.contestDate.getTime(), contestDate.getTime());
+  });
+
+  it('should create a categorized contest and retrieve its category', function() {
+    var newContest = createContest();
+    assert.equal(newContest.category, 'Advanced');
   });
 
 });
